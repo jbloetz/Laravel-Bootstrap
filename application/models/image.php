bc8d5aa04a8d17328a22aeed870dccf447403f93
David Thorpe <davzie@davzie.com>
2012-04-11 16:43:06 +0100

    Fixed incorrect function name in Images model
    Signed-off-by: David Thorpe <davzie@davzie.com>

--- application/models/image.php
+++ application/models/image.php
@@ -4,7 +4,7 @@ class Image extends Eloquent {
 	public static $timestamps = true;
 	public static $table = 'gallery_images';
 
-	public function page()
+	public function gallery()
 	{
 		return $this->belongs_to('Gallery');
 	}
@@ -17,5 +17,9 @@ class Image extends Eloquent {
 	public function user()
 	{
 		return $this->belongs_to('User');
+	}
+	public function uploads()
+	{
+		return $this->has_many('Upload', 'link_id')->where('link_type', '=', 'image');
 	}
 }
